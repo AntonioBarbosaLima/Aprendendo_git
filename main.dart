de41abab23367f258c4909159a43a86b2399b0cd
Amanda Victoria <amandamelquiades01@gmail.com>
2022-01-29 21:16:03 -0300

    funcionario vendedor

--- main.dart
+++ main.dart
@@ -1,3 +1,16 @@
+import 'vendedor.dart';
+
 void main() {
-  
+  Vendedor vendedor = Vendedor('Isabella', 15, 1400, 'rua da solidao');
+  print(vendedor);
+  vendedor.trabalhar();
+  print(vendedor);
+  dadosVendedor(vendedor);
+}
+
+void dadosVendedor(Vendedor vendedor) {
+  print("Nome do vendedor: ${vendedor.nome}");
+  print("Idade do vendedor: ${vendedor.idade}");
+  print("Salario do vendedor: ${vendedor.salario}");
+  print("Endereço do vendedor: ${vendedor.endereco}");
 }
